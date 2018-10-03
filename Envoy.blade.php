276b52b2c1882b0d75c8d49f6e33695ed573a578
Graham <graham@responseconcepts.com>
2018-10-03 10:25:27 +0200

    changed envoy commands

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,11 +1,16 @@
 @servers(['web' => ['root@128.199.63.237']])
 
 @task('deploy', ['on' => 'web'])
+    {{-- go to the right directory --}}
     cd /var/git/directevents
 
-    @if ($branch)
-        git pull origin {{ $branch }}
-    @endif
+    {{-- pull master branch --}}
+    git pull origin master
 
+    {{-- run migrations --}}
     php artisan migrate
+@endtask
+
+@task('foo', ['on' => 'web'])
+    ls -la
 @endtask
